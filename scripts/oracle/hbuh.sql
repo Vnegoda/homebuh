fd41a839590866ce7d9ae12e214aa0069e676d3b
vnegoda <negodaukrlggit@gmail.com>
2015-11-15 00:46:45 +0200

    add sp spr_kurs, spr_valut

--- scripts/oracle/hbuh.sql
+++ scripts/oracle/hbuh.sql
@@ -1,6 +1,6 @@
 --------------------------------------------
 -- Export file for user HBUH              --
--- Created by Vova on 14.11.2015, 0:29:15 --
+-- Created by Vova on 15.11.2015, 0:45:49 --
 --------------------------------------------
 
 set define off
@@ -286,6 +286,125 @@ prompt
 create or replace force view hbuh.v_spr_valut as
 select id,cod,short_name,name
     from hbuh.spr_valut;
+
+prompt
+prompt Creating procedure SPR_CATEGORY_INS
+prompt ===================================
+prompt
+create or replace procedure hbuh.spr_category_ins(v_id_parent in number,
+                                             v_is_rashod in number,
+                                             v_name      in varchar2,
+                                             v_nout      out number,
+                                             v_sout      out varchar2
+                                             
+                                             ) is
+  -- nva 2015.10.28
+  -- insert values in spr_category
+begin
+  declare
+    v_cnt number;
+    is_empty_values       exception;
+    is_null_values        exception;
+    is_not_correct_values exception;
+    is_dublicate_record   exception;
+    is_dublicate_name     exception;
+    is_not_exist_parent   exception;
+  begin
+    v_cnt  := 0;
+    v_nout := 0;
+    v_sout := 'Операция не выполнена';
+    ----------- valid input values
+    -- valid null
+    if v_id_parent is null or v_is_rashod is null or v_name is null then
+      raise is_null_values;
+    end if;
+  
+    -- valid empty
+    if v_id_parent = 0 then
+      raise is_empty_values;
+    end if;
+  
+    if length(trim(v_name)) = 0 then
+      raise is_empty_values;
+    end if;
+    if v_is_rashod != 0 and v_is_rashod != 1 then
+      raise is_not_correct_values;
+    end if;
+    -- valid parent category
+    if v_id_parent > 0 then
+      v_cnt := 0;
+      select count(1)
+        into v_cnt
+        from hbuh.spr_category
+       where id = v_id_parent;
+      if v_cnt <> 0 then
+        raise is_not_exist_parent;
+      end if;
+    end if;
+    -- valid dublicat
+    v_cnt := 0;
+    select count(1)
+      into v_cnt
+      from hbuh.spr_category
+     where id_parent = v_id_parent
+       and is_rashod = v_is_rashod
+       and name = upper(trim(v_name));
+    if v_cnt <> 0 then
+      raise is_dublicate_record;
+    end if;
+  
+    v_cnt := 0;
+    select count(1)
+      into v_cnt
+      from hbuh.spr_category
+     where name = upper(trim(v_name));
+    if v_cnt <> 0 then
+      raise is_dublicate_name;
+    end if;
+    -- insert values
+    insert into hbuh.spr_category
+      (id, id_parent, is_rashod, name)
+    values
+      (hbuh.seq_spr_category.nextval,
+       v_id_parent,
+       v_is_rashod,
+       upper(trim(v_name)));
+  
+    commit;
+    v_nout := 1;
+    v_sout := 'Операция  выполнена';
+  exception
+    when is_empty_values then
+      v_nout := -1;
+      v_sout := 'Есть незаполненные значения. Операция не выполнена';
+      rollback;
+    when is_null_values then
+      v_nout := -2;
+      v_sout := 'Есть поля содержащие значение null. Операция не выполнена';
+      rollback;
+    when is_dublicate_record then
+      v_nout := -3;
+      v_sout := 'Уже есть такая категория. Операция не выполнена';
+      rollback;
+    when is_not_correct_values then
+      v_nout := -4;
+      v_sout := 'Некорректный признак операции (доход/расход). Операция не выполнена';
+      rollback;
+    when is_not_exist_parent then
+      v_nout := -5;
+      v_sout := 'Не найдена родительская категория. Операция не выполнена';
+      rollback;
+    when is_dublicate_name then
+      v_nout := -6;
+      v_sout := 'Уже есть категория с таким названием. Операция не выполнена';
+      rollback;
+    when others then
+      v_nout := SQLCODe;
+      v_sout := SUBSTR(SQLERRM, 1, 100);
+      rollback;
+  end;
+end spr_category_ins;
+/
 
 prompt
 prompt Creating procedure SPR_COUNTS_DEL
@@ -630,6 +749,257 @@ begin
       rollback;
   end;
 end spr_counts_upd;
+/
+
+prompt
+prompt Creating procedure SPR_KURS_DEL
+prompt ===============================
+prompt
+create or replace procedure hbuh.spr_kurs_del(v_id   in number,
+                                         v_nout out number,
+                                         v_sout out varchar2
+                                         
+                                         ) is
+  -- nva 2015.10.28
+  -- delete values in spr_kurs
+begin
+  declare
+    v_cnt number;
+    is_empty_values exception;
+    is_null_values  exception;
+    is_not_exists   exception;
+  
+  begin
+    v_cnt  := 0;
+    v_nout := 0;
+    v_sout := 'Операция не выполнена';
+    ----------- valid input values
+    -- valid null
+    if v_id is null then
+      raise is_null_values;
+    end if;
+  
+    --valid exists
+    v_cnt := 0;
+    select count(1) into v_cnt from hbuh.spr_kurs where id = v_id;
+    if v_cnt = 0 then
+      raise is_not_exists;
+    end if;
+  
+    -- valid empty
+    if v_id = 0 then
+      raise is_empty_values;
+    end if;
+  
+    -- update values
+    delete from hbuh.spr_kurs where id = v_id;
+  
+    commit;
+    v_nout := 1;
+    v_sout := 'Операция  выполнена';
+  exception
+    when is_empty_values then
+      v_nout := -1;
+      v_sout := 'Есть незаполненные значения. Операция не выполнена';
+      rollback;
+    when is_null_values then
+      v_nout := -2;
+      v_sout := 'Есть поля содержащие значение null. Операция не выполнена';
+      rollback;
+    
+    when is_not_exists then
+      v_nout := -4;
+      v_sout := 'Удаляемые данные не найдены. Операция отменена';
+      rollback;
+    when others then
+      v_nout := SQLCODe;
+      v_sout := SUBSTR(SQLERRM, 1, 100);
+      rollback;
+  end;
+end spr_kurs_del;
+/
+
+prompt
+prompt Creating procedure SPR_KURS_INS
+prompt ===============================
+prompt
+create or replace procedure hbuh.spr_kurs_ins(v_date        in date,
+                                         v_id_valuta1  in number,
+                                         v_id_valuta2  in NUMBER,
+                                         v_sum_valuta1 in number,
+                                         v_sum_valuta2 in number,
+                                         v_nout        out number,
+                                         v_sout        out varchar2
+                                         
+                                         ) is
+  -- nva 2015.10.28
+  -- insert values in spr_kurs
+begin
+  declare
+    v_cnt number;
+    is_empty_values     exception;
+    is_null_values      exception;
+    is_dublicate_record exception;
+  begin
+    v_cnt  := 0;
+    v_nout := 0;
+    v_sout := 'Операция не выполнена';
+    ----------- valid input values
+    -- valid null
+    if v_date is null or v_id_valuta1 is null or v_id_valuta2 is null or
+       v_sum_valuta1 is null or v_sum_valuta2 is null then
+      raise is_null_values;
+    end if;
+  
+    -- valid empty
+    if v_id_valuta1 = 0 or v_id_valuta2 = 0 or v_sum_valuta1 = 0 or
+       v_sum_valuta1 = 0 then
+      raise is_empty_values;
+    end if;
+  
+    -- valid dublicat
+    v_cnt := 0;
+    select count(1)
+      into v_cnt
+      from hbuh.spr_kurs
+     where date_kurs = v_date
+       and id_valuta1 = v_id_valuta1
+       and id_valuta2 = v_id_valuta2;
+    if v_cnt <> 0 then
+      raise is_dublicate_record;
+    end if;
+  
+    -- insert values
+    insert into hbuh.spr_kurs
+      (id, date_kurs, id_valuta1, id_valuta2, sum_valuta1, sum_valuta2)
+    values
+      (hbuh.seq_spr_kurs.nextval,
+       v_date,
+       v_id_valuta1,
+       v_id_valuta2,
+       v_sum_valuta1,
+       v_sum_valuta2);
+  
+    commit;
+    v_nout := 1;
+    v_sout := 'Операция  выполнена';
+  exception
+    when is_empty_values then
+      v_nout := -1;
+      v_sout := 'Есть незаполненные значения. Операция не выполнена';
+      rollback;
+    when is_null_values then
+      v_nout := -2;
+      v_sout := 'Есть поля содержащие значение null. Операция не выполнена';
+      rollback;
+    when is_dublicate_record then
+      v_nout := -3;
+      v_sout := 'На эту дату для этих валют уже есть заданный курс. Операция не выполнена';
+      rollback;
+    when others then
+      v_nout := SQLCODe;
+      v_sout := SUBSTR(SQLERRM, 1, 100);
+      rollback;
+  end;
+end spr_kurs_ins;
+/
+
+prompt
+prompt Creating procedure SPR_KURS_UPD
+prompt ===============================
+prompt
+create or replace procedure hbuh.spr_kurs_upd(v_id          in number,
+                                         v_date        in date,
+                                         v_id_valuta1  in number,
+                                         v_id_valuta2  in NUMBER,
+                                         v_sum_valuta1 in number,
+                                         v_sum_valuta2 in number,
+                                         v_nout        out number,
+                                         v_sout        out varchar2
+                                         
+                                         ) is
+  -- nva 2015.10.28
+  -- update values in spr_kurs
+begin
+  declare
+    v_cnt number;
+    is_empty_values     exception;
+    is_null_values      exception;
+    is_dublicate_record exception;
+    is_not_exists       exception;
+  
+  begin
+    v_cnt  := 0;
+    v_nout := 0;
+    v_sout := 'Операция не выполнена';
+    ----------- valid input values
+    -- valid null
+    if v_date is null or v_id_valuta1 is null or v_id_valuta2 is null or
+       v_sum_valuta1 is null or v_sum_valuta2 is null or v_id is null then
+      raise is_null_values;
+    end if;
+  
+    --valid exists
+    v_cnt := 0;
+    select count(1) into v_cnt from hbuh.spr_kurs where id = v_id;
+    if v_cnt = 0 then
+      raise is_not_exists;
+    end if;
+  
+    -- valid empty
+    if v_id_valuta1 = 0 or v_id_valuta2 = 0 or v_sum_valuta1 = 0 or
+       v_sum_valuta1 = 0 or v_id = 0 then
+      raise is_empty_values;
+    end if;
+  
+    -- valid dublicat
+    v_cnt := 0;
+    select count(1)
+      into v_cnt
+      from hbuh.spr_kurs
+     where id != v_id
+       and date_kurs = v_date
+       and id_valuta1 = v_id_valuta1
+       and id_valuta2 = v_id_valuta2;
+    if v_cnt <> 0 then
+      raise is_dublicate_record;
+    end if;
+  
+    -- update values
+    update hbuh.spr_kurs
+       set date_kurs   = v_date,
+           id_valuta1  = v_id_valuta1,
+           id_valuta2  = v_id_valuta2,
+           sum_valuta1 = v_sum_valuta1,
+           sum_valuta2 = v_sum_valuta2
+     where id = v_id;
+  
+    commit;
+    v_nout := 1;
+    v_sout := 'Операция  выполнена';
+  exception
+    when is_empty_values then
+      v_nout := -1;
+      v_sout := 'Есть незаполненные значения. Операция не выполнена';
+      rollback;
+    when is_null_values then
+      v_nout := -2;
+      v_sout := 'Есть поля содержащие значение null. Операция не выполнена';
+      rollback;
+    when is_dublicate_record then
+      v_nout := -3;
+      v_sout := 'На эту дату для этих валют уже есть заданный курс. Операция не выполнена';
+      rollback;
+    when is_not_exists then
+      v_nout := -4;
+      v_sout := 'Редактируемые данные не найдены. Операция отменена';
+      rollback;
+    when others then
+      v_nout := SQLCODe;
+      v_sout := SUBSTR(SQLERRM, 1, 100);
+      rollback;
+  end;
+end spr_kurs_upd;
 /
 
 prompt
