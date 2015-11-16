a4499cad5c0d7a02622f0f6f504210627c12d59e
vnegoda <negodaukrlggit@gmail.com>
2015-11-17 00:13:30 +0200

    add sp categ and operation_ins

--- scripts/oracle/hbuh.sql
+++ scripts/oracle/hbuh.sql
@@ -1,6 +1,6 @@
 --------------------------------------------
 -- Export file for user HBUH              --
--- Created by Vova on 15.11.2015, 0:45:49 --
+-- Created by Vova on 17.11.2015, 0:12:06 --
 --------------------------------------------
 
 set define off
@@ -18,7 +18,7 @@ create table HBUH.OPERATION
   id_count_end   NUMBER not null,
   summa_begin    NUMBER(10,3),
   summ_end       NUMBER(10,3),
-  id_kateg       NUMBER not null
+  id_categ       NUMBER not null
 )
 tablespace HBUH
   pctfree 10
@@ -38,7 +38,7 @@ comment on column HBUH.OPERATION.summa_begin
   is 'сумма со счета источника снимаемая';
 comment on column HBUH.OPERATION.summ_end
   is 'сумма на счет получатель добавленная';
-comment on column HBUH.OPERATION.id_kateg
+comment on column HBUH.OPERATION.id_categ
   is 'тип операции';
 alter table HBUH.OPERATION
   add constraint KEY_ID_OPER unique (ID)
@@ -288,6 +288,199 @@ select id,cod,short_name,name
     from hbuh.spr_valut;
 
 prompt
+prompt Creating procedure OPERATION_INS
+prompt ================================
+prompt
+create or replace procedure hbuh.operation_ins(v_id_user        in number,
+                                          v_id_count_begin in number,
+                                          v_id_count_end   in number,
+                                          v_summa_begin    in number,
+                                          v_summa_end      in number,
+                                          v_nout           out number,
+                                          v_sout           out varchar2
+                                          
+                                          ) is
+  -- last edit
+  -- nva 
+  -- 2015.11.17
+  -- insert values in operatioin
+  -- when operation MOVE: v_id_count_begin and v_id_count_end >0 and v_summa_begin and v_summa_end >0
+  -- other opertion v_id_count_begin and v_summa_begin>0, but v_summa_begin and v_summa_end =0 
+begin
+  declare
+    v_cnt number;
+    is_empty_values     exception;
+    is_null_values      exception;
+    is_dublicate_record exception;
+    is_not_exist_user   exception;
+    is_not_exist_valut  exception;
+  begin
+    v_cnt  := 0;
+    v_nout := 0;
+    v_sout := 'Операция не выполнена';
+    ----------- valid input values
+    -- valid null
+    if v_id_user is null then
+      raise is_null_values;
+    end if;
+  
+    if v_name is null then
+      raise is_null_values;
+    end if;
+  
+    if v_id_valut is null then
+      raise is_null_values;
+    end if;
+    -- valid empty
+    if v_id_user = 0 then
+      raise is_empty_values;
+    end if;
+  
+    if v_id_valut = 0 then
+      raise is_empty_values;
+    end if;
+  
+    if length(trim(v_name)) = 0 then
+      raise is_empty_values;
+    end if;
+    -- valit exist value
+    v_cnt := 0;
+    select count(1) into v_cnt from hbuh.users where id = v_id_user;
+    if v_cnt != 0 then
+      raise is_not_exist_user;
+    end if;
+    --
+    v_cnt := 0;
+    select count(1) into v_cnt from hbuh.spr_valut where id = v_id_valut;
+    if v_cnt != 0 then
+      raise is_not_exist_valut;
+    end if;
+  
+    -- valid dublicat
+    v_cnt := 0;
+    select count(1)
+      into v_cnt
+      from hbuh.spr_counts
+     where name = upper(v_name);
+    if v_cnt <> 0 then
+      raise is_dublicate_record;
+    end if;
+  
+    -- insert values
+    insert into hbuh.spr_counts
+      (id, id_user, name, id_valut)
+    values
+      (hbuh.seq_spr_counts.nextval,
+       v_id_user,
+       upper(trim(v_name)),
+       v_id_valut);
+  
+    commit;
+    v_nout := 1;
+    v_sout := 'Операция  выполнена';
+  exception
+    when is_empty_values then
+      v_nout := -1;
+      v_sout := 'Есть незаполненные значения. Операция не выполнена';
+      rollback;
+    when is_null_values then
+      v_nout := -2;
+      v_sout := 'Есть поля содержащие значение null. Операция не выполнена';
+      rollback;
+    when is_dublicate_record then
+      v_nout := -3;
+      v_sout := 'Уже есть счет с таким названием. Операция не выполнена';
+      rollback;
+    when is_not_exist_user then
+      v_nout := -4;
+      v_sout := 'Заданный пользователь не найден. Операция не выполнена';
+      rollback;
+    when is_not_exist_valut then
+      v_nout := -5;
+      v_sout := 'Заданная валюта не найдена. Операция не выполнена';
+      rollback;
+    when others then
+      v_nout := SQLCODe;
+      v_sout := SUBSTR(SQLERRM, 1, 100);
+      rollback;
+  end;
+end operation_ins;
+/
+
+prompt
+prompt Creating procedure SPR_CATEGORY_DEL
+prompt ===================================
+prompt
+create or replace procedure hbuh.spr_category_del(v_id   in number,
+                                             v_nout out number,
+                                             v_sout out varchar2
+                                             
+                                             ) is
+  -- nva 2015.10.28
+  -- delete values in spr_category
+begin
+  declare
+    v_cnt number;
+  
+    v_id_parent number;
+  
+    is_categ_used exception;
+  
+    is_not_exist_category exception;
+  
+  begin
+    v_id_parent := 0;
+    v_cnt       := 0;
+    v_nout      := 0;
+    v_sout      := 'Операция не выполнена';
+    ----------- valid input values
+    v_cnt := 0;
+    select count(1) into v_cnt from hbuh.spr_category where id = v_id;
+    if v_cnt != 0 then
+      raise is_not_exist_category;
+    end if;
+  
+    -- valid exist use category in operation and invert flag rashod
+    v_cnt := 0;
+    select count(1) into v_cnt from hbuh.operation where id_categ = v_id;
+    if v_cnt <> 0 then
+      raise is_categ_used;
+    
+    end if;
+  
+    --replace id_parent for child category, where parent this  category
+    select id_parent
+      into v_id_parent
+      from hbuh.spr_category
+     where id = v_id;
+    update hbuh.spr_category
+       set id_parent = v_id_parent
+     where id_parent = v_id;
+    -- delete values
+    delete from hbuh.spr_category where id = v_id;
+  
+    commit;
+    v_nout := 1;
+    v_sout := 'Операция  выполнена';
+  exception
+    when is_not_exist_category then
+      v_nout := -7;
+      v_sout := 'Изменяемая категория не найдена. Операция не выполнена';
+      rollback;
+    
+    when is_categ_used then
+      v_nout := -8;
+      v_sout := 'По данной категории уже были движения, удалять нельзя. Операция не выполнена';
+      rollback;
+    when others then
+      v_nout := SQLCODe;
+      v_sout := SUBSTR(SQLERRM, 1, 100);
+      rollback;
+  end;
+end spr_category_del;
+/
+
+prompt
 prompt Creating procedure SPR_CATEGORY_INS
 prompt ===================================
 prompt
@@ -407,6 +600,156 @@ end spr_category_ins;
 /
 
 prompt
+prompt Creating procedure SPR_CATEGORY_UPD
+prompt ===================================
+prompt
+create or replace procedure hbuh.spr_category_upd(v_id        in number,
+                                             v_id_parent in number,
+                                             v_is_rashod in number,
+                                             v_name      in varchar2,
+                                             v_nout      out number,
+                                             v_sout      out varchar2
+                                             
+                                             ) is
+  -- nva 2015.10.28
+  -- insert values in spr_category
+begin
+  declare
+    v_cnt           number;
+    v_is_rashod_old number(1);
+  
+    is_empty_values       exception;
+    is_null_values        exception;
+    is_not_correct_values exception;
+    is_dublicate_record   exception;
+    is_dublicate_name     exception;
+    is_not_exist_parent   exception;
+    is_not_exist_category exception;
+    cant_edit_flag_rashod exception;
+  begin
+    v_cnt  := 0;
+    v_nout := 0;
+    v_sout := 'Операция не выполнена';
+    ----------- valid input values
+    v_cnt := 0;
+    select count(1) into v_cnt from hbuh.spr_category where id = v_id;
+    if v_cnt != 0 then
+      raise is_not_exist_category;
+    end if;
+  
+    -- valid null
+    if v_id_parent is null or v_is_rashod is null or v_name is null then
+      raise is_null_values;
+    end if;
+  
+    -- valid empty
+    if v_id_parent = 0 then
+      raise is_empty_values;
+    end if;
+  
+    if length(trim(v_name)) = 0 then
+      raise is_empty_values;
+    end if;
+    if v_is_rashod != 0 and v_is_rashod != 1 then
+      raise is_not_correct_values;
+    end if;
+    -- valid exist use category in operation and invert flag rashod
+    v_cnt := 0;
+    select count(1) into v_cnt from hbuh.operation where id_categ = v_id;
+    if v_cnt <> 0 then
+      --if this kateg used
+      select is_rashod
+        into v_is_rashod_old
+        from hbuh.spr_category
+       where id = v_id;
+      if v_is_rashod_old != v_is_rashod then
+        raise cant_edit_flag_rashod;
+      end if;
+    end if;
+  
+    -- valid parent category
+    if v_id_parent > 0 then
+      v_cnt := 0;
+      select count(1)
+        into v_cnt
+        from hbuh.spr_category
+       where id = v_id_parent;
+      if v_cnt <> 0 then
+        raise is_not_exist_parent;
+      end if;
+    end if;
+    -- valid dublicat
+    v_cnt := 0;
+    select count(1)
+      into v_cnt
+      from hbuh.spr_category
+     where id != v_id
+       and id_parent = v_id_parent
+       and is_rashod = v_is_rashod
+       and name = upper(trim(v_name));
+    if v_cnt <> 0 then
+      raise is_dublicate_record;
+    end if;
+  
+    v_cnt := 0;
+    select count(1)
+      into v_cnt
+      from hbuh.spr_category
+     where id != v_id
+       and name = upper(trim(v_name));
+    if v_cnt <> 0 then
+      raise is_dublicate_name;
+    end if;
+    -- update values
+    update hbuh.spr_category
+       set id_parent = v_id_parent, is_rashod = v_is_rashod, name = v_name
+     where id = v_id;
+  
+    commit;
+    v_nout := 1;
+    v_sout := 'Операция  выполнена';
+  exception
+    when is_empty_values then
+      v_nout := -1;
+      v_sout := 'Есть незаполненные значения. Операция не выполнена';
+      rollback;
+    when is_null_values then
+      v_nout := -2;
+      v_sout := 'Есть поля содержащие значение null. Операция не выполнена';
+      rollback;
+    when is_dublicate_record then
+      v_nout := -3;
+      v_sout := 'Уже есть такая категория. Операция не выполнена';
+      rollback;
+    when is_not_correct_values then
+      v_nout := -4;
+      v_sout := 'Некорректный признак операции (доход/расход). Операция не выполнена';
+      rollback;
+    when is_not_exist_parent then
+      v_nout := -5;
+      v_sout := 'Не найдена родительская категория. Операция не выполнена';
+      rollback;
+    when is_dublicate_name then
+      v_nout := -6;
+      v_sout := 'Уже есть категория с таким названием. Операция не выполнена';
+      rollback;
+    when is_not_exist_category then
+      v_nout := -7;
+      v_sout := 'Изменяемая категория не найдена. Операция не выполнена';
+      rollback;
+    when cant_edit_flag_rashod then
+      v_nout := -8;
+      v_sout := 'По данной категории уже были движения, изменять признак расхода нельзя. Операция не выполнена';
+      rollback;
+    when others then
+      v_nout := SQLCODe;
+      v_sout := SUBSTR(SQLERRM, 1, 100);
+      rollback;
+  end;
+end spr_category_upd;
+/
+
+prompt
 prompt Creating procedure SPR_COUNTS_DEL
 prompt =================================
 prompt
@@ -455,7 +798,7 @@ begin
       raise is_count_used;
     end if;
   
-    -- insert values
+    -- delete values
     delete from hbuh.spr_counts where id = v_id;
   
     commit;
